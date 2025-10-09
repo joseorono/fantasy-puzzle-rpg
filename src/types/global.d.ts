22f6ac48f30210221bbf3eb6189fb0605ab84b5b
José Miguel Oroño <joseomaker@gmail.com>
2025-10-08 23:42:36 -0500

    Recycle types from old game

---
+++ src/types/global.d.ts
@@ -0,0 +1,5 @@
+// Generic for anything that can be null
+type Nullable<T> = T | null;
+
+// Anything that can be rendered in a React component
+type RenderableElement = JSX.Element | string | null;
---
+++ src/types/number-types.ts
@@ -0,0 +1,10 @@
+import * as z from "zod"; // This leads to a smaller bundle size somehow
+
+export const integer = z.number().int().brand('Integer');
+export type Integer = z.infer<typeof integer>;
+
+export const positiveNumber = z.number().positive().brand('PositiveNumber');
+export type PositiveNumber = z.infer<typeof positiveNumber>;
+
+export const negativeNumber = z.number().negative().brand('NegativeNumber');
+export type NegativeNumber = z.infer<typeof negativeNumber>;
