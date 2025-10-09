6df9ab572899e371866bbeaac1c5db1bfafc9586
José Miguel Oroño <joseomaker@gmail.com>
2025-10-08 22:23:35 -0500

    Add Tailwind CSS and update project config
    Added Tailwind CSS and @tailwindcss/vite dependencies, updated .prettierignore to exclude node_modules, and made related configuration changes in vite.config.ts, App.tsx, and index.css to enable Tailwind CSS integration.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,10 @@ function App() {
         <a href="https://vite.dev" target="_blank">Vite</a>
         <a href="https://react.dev" target="_blank">React</a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold bg-red-500">Vite + React</h1>
+      <h2 className="text-2xl font-bold underline">
+        Hello world!
+      </h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -19,9 +22,10 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-center">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   );
 }
