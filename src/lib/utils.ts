f58c0f91cc44138ed28682e12f8da06ebf7ac4cd
José Miguel Oroño <joseomaker@gmail.com>
2025-10-19 23:21:35 -0500

    Add noop() function and add JSDoc to auxObjectMap
    Introduced a noop function and a documented auxObjectMap function to apply a mapping function to object values. These utilities enhance the general-purpose helper functions available in utils.ts.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const noop = () => {};
+
 export function shuffleArray(argArray: any[]) {
   // Durstenfeld shuffle array
   let array = argArray.slice(); // Copy the array, don't mutate the original
@@ -19,6 +21,15 @@ export function auxSleepFor(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, Math.floor(ms)));
 }
 
+
+/**
+ * Applies a function to each element of an object and returns a new object.
+ *
+ * @param {Record<string, any>} object - The object to map over.
+ * @param {(arg: any) => any} mapFn - The function to apply to each element.
+ * @returns {Record<string, any>} - A new object with the same keys as the input
+ * object, but with the values transformed by the map function.
+ */
 export function auxObjectMap(object: Record<string, any>, mapFn: (arg: any) => any): Record<string, any> {
   return Object.keys(object).reduce(function (result: any, key: any) {
     result[key] = mapFn(object[key]);
