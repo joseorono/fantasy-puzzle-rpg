4c4401a6cfa5dcf780e843d4253d28555c62dcfe
José Miguel Oroño <joseomaker@gmail.com>
2025-10-26 21:36:46 -0500

    Set initial coins value to 10
    Updated INITIAL_RESOURCES_STATE to start with 10 coins instead of 0. This change may help with initial gameplay balance or testing.

--- src/constants/resources.ts
+++ src/constants/resources.ts
@@ -8,7 +8,7 @@ import type { Resources } from '~/types/resources';
  * Initial resources state
  */
 export const INITIAL_RESOURCES_STATE: Resources = {
-  coins: 0,
+  coins: 10,
   gold: 0,
   copper: 0,
   silver: 0,
