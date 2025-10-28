d173a202ad7c9bc4b26aa312a18d334746eb8f29
José Miguel Oroño <joseomaker@gmail.com>
2025-10-27 22:30:59 -0500

    Add consumable items to inventory constants
    Introduced ConsumableItems array with sample items and their properties. Also added type imports for item data and a placeholder for equipment items.

--- src/constants/inventory.ts
+++ src/constants/inventory.ts
@@ -2,4 +2,37 @@
  * Inventory and items constants
  */
 
+import type { ConsumableItemData, EquipmentItemData } from "~/types";
+
 export const MAX_AMOUNT_PER_ITEM = 99;
+
+/*
+ * Items
+ */
+export const EquipmentItems: EquipmentItemData[] = [
+
+];
+
+export const ConsumableItems: ConsumableItemData[] = [
+  {
+    id: 'potion',
+    name: 'Potion',
+    type: 'consumable',
+    description: 'Heals a little. Tastes like red.',
+    amount: 100,
+  },
+  {
+    id: 'high-potion',
+    name: 'High Potion',
+    type: 'consumable',
+    description: 'Heals a lot. Tastes like really red.',
+    amount: 500,
+  },
+  {
+    id: 'energy-booster',
+    name: 'Energy Booster',
+    type: 'consumable',
+    description: 'A jolt for the whole team. Fills 30% of the ultimate bar.',
+    amount: 30,
+  },
+];
