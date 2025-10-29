e6a185cfce09081f6fa28a533f167e313d68d2f6
José Miguel Oroño <joseomaker@gmail.com>
2025-10-29 03:29:13 -0500

    Remove unused sound constants

--- src/constants/audio.ts
+++ src/constants/audio.ts
@@ -3,7 +3,6 @@ export enum SoundNames {
   bgNoiseMiner = 'bgNoiseMiner',
   bgNoiseFarmer = 'bgNoiseFarmer',
   bgNoiseForum = 'bgNoiseForum',
-  closeModal = 'closeModal',
   clickChangeTab = 'clickChangeTab',
   clickCoin = 'clickCoin',
 }
@@ -12,7 +11,6 @@ export const soundFiles = {
   [SoundNames.bgNoiseMiner]: 'assets/audio/bg-noise/miner.mp3',
   [SoundNames.bgNoiseFarmer]: 'assets/audio/bg-noise/farmer.mp3',
   [SoundNames.bgNoiseForum]: 'assets/audio/bg-noise/forum.mp3',
-  [SoundNames.closeModal]: 'assets/audio/closeModal.mp3',
   [SoundNames.clickChangeTab]: 'assets/audio/click-change-tab.mp3',
   [SoundNames.clickCoin]: 'assets/audio/click-coin.mp3',
 };
