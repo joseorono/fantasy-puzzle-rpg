1152c7a21da917c19af4a07b22e6c5b1deaf5186
José Miguel Oroño <joseomaker@gmail.com>
2025-10-29 03:59:25 -0500

    Update import paths to use tilde alias
    Changed import statements in App.tsx to use the tilde (~) alias for module paths instead of relative paths for improved readability and maintainability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-//import BattleScreen from "./views/battle-screen";
-//import ResourcesTestView from "./views/resources-test";
-import MouseTracker from "./components/effects/mouse-tracker";
-import InventoryTestView from "./views/inventory-test";
-import ResourcesTestView from "./views/resources-test";
-import SoundTestView from "./views/sound-test";
-import TestView from "./views/test-view";
+//import BattleScreen from "~/views/battle-screen";
+import MouseTracker from "~/components/effects/mouse-tracker";
+import InventoryTestView from "~/views/inventory-test";
+import ResourcesTestView from "~/views/resources-test";
+import SoundTestView from "~/views/sound-test";
+import TestView from "~/views/test-view";
 
 function App() {
   return (
