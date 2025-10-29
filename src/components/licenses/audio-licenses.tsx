7ed90c185475c45bff306d89ab6ffd1f05280e7b
José Miguel Oroño <joseomaker@gmail.com>
2025-10-29 03:26:14 -0500

    Create audio-licenses.tsx

---
+++ src/components/licenses/audio-licenses.tsx
@@ -0,0 +1,63 @@
+export default function AudioLicensesDialogContent() {
+  return (
+    <>
+      <div className="text-large flex flex-col justify-center gap-2 overflow-y-auto">
+        <h1 className="text-3xl">CREDITS</h1>
+
+        <h1 className="text-3xl" id="licences-and-credits">
+          Licences and Credits
+        </h1>
+
+        <h2 className="text-2xl" id="audio-assets-licences-and-credits-">
+          Audio Assets - Licences and Credits
+        </h2>
+
+        <h3 className="text-xl" id="sound-effects-">
+          Sound Effects
+        </h3>
+
+        <p>
+          Close modal sound, Creative Commons 0, modusmogulus&nbsp;
+          <a href="https://freesound.org/people/modusmogulus/sounds/735897/">
+            https://freesound.org/people/modusmogulus/sounds/735897/
+          </a>
+        </p>
+
+        <p>
+          Really clean clicking sound (click-change-tab), Creative Commons 0, BiORNADE&nbsp;
+          <a href="https://freesound.org/people/BiORNADE/sounds/735804/">
+            https://freesound.org/people/BiORNADE/sounds/735804/
+          </a>
+        </p>
+
+        <p>
+          Coin click (click-coin), Creative Commons 0, The_Frisbee_of_Peace&nbsp;
+          <a href="https://freesound.org/people/The_Frisbee_of_Peace/sounds/575576/">
+            https://freesound.org/people/The_Frisbee_of_Peace/sounds/575576/
+          </a>
+        </p>
+
+        <p>
+          Miner background noise (bgNoiseMiner) — Pickaxe 1/4, CC Attribution 4.0, TechspiredMinds&nbsp;
+          <a href="https://freesound.org/people/TechspiredMinds/sounds/728756/">
+            https://freesound.org/people/TechspiredMinds/sounds/728756/
+          </a>
+        </p>
+
+        <p>
+          Farmer background noise (bgNoiseFarmer) — 01130 chicken hen 2.wav, CC Attribution 4.0, Robinhood76&nbsp;
+          <a href="https://freesound.org/people/Robinhood76/sounds/75726/">
+            https://freesound.org/people/Robinhood76/sounds/75726/
+          </a>
+        </p>
+
+        <p>
+          Forum background noise (bgNoiseForum) — Crowd.wav, CC Attribution 4.0, Haykrich&nbsp;
+          <a href="https://freesound.org/people/Haykrich/sounds/401560/">
+            https://freesound.org/people/Haykrich/sounds/401560/
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
