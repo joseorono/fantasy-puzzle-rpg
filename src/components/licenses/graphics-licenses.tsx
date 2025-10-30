402e944f6e862a536252c477fba38c80ddf781e9
José Miguel Oroño <joseomaker@gmail.com>
2025-10-30 03:32:07 -0500

    Add Innkeeper and Witch portraits with licenses
    Added new portrait images for Innkeeper and Witch to the assets folder. Introduced a graphics licenses dialog component listing credits and license information for these assets.

---
+++ src/components/licenses/graphics-licenses.tsx
@@ -0,0 +1,31 @@
+export default function GraphicsLicensesDialogContent() {
+  return (
+    <>
+      <div className="text-large flex flex-col justify-center gap-2 overflow-y-auto">
+
+        <h2 className="text-2xl" id="graphics-assets-licences-and-credits-">
+          Graphics Assets - Licences and Credits
+        </h2>
+
+        <h3 className="text-xl" id="graphics-assets-licences-and-credits-">
+          Graphics
+        </h3>
+
+        <p>
+          Innkeeper portrait, CC-BY 3.0, JosephSeraph&nbsp;
+          <a href="https://opengameart.org/content/js-actor-innkeeper">
+            https://opengameart.org/content/js-actor-innkeeper
+          </a>
+        </p>
+
+        <p>
+          Witch portrait, CC-BY 3.0, JosephSeraph&nbsp;
+          <a href="https://opengameart.org/content/js-actor-witch">
+            https://opengameart.org/content/js-actor-witch
+          </a>
+        </p>
+
+      </div>
+    </>
+  );
+}
