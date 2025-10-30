a9fb100042e1947a1573d706fac06d8d1520ec88
José Miguel Oroño <joseomaker@gmail.com>
2025-10-30 03:34:40 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 //import BattleScreen from "~/views/battle-screen";
+//import { DialogueTestView } from "~/views/dialogue-test";
 import MouseTracker from "~/components/effects/mouse-tracker";
 import InventoryTestView from "~/views/inventory-test";
 import ResourcesTestView from "~/views/resources-test";
@@ -13,6 +14,7 @@ function App() {
     <InventoryTestView />
     <ResourcesTestView />
     <SoundTestView />
+    {/* <DialogueTestView /> */}
     {/* <BattleScreen /> */}
   </>
   );
