7f48ec19430331d526aa973ecf229438d64eaeaf
Andreina Di Brigida <Akiarakirax3@gmail.com>
2025-11-01 16:03:59 -0400

    Update App.tsx
    Co-Authored-By: José Miguel Oroño <joseomaker@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-//import BattleScreen from "~/views/battle-screen";
-//import { DialogueTestView } from "~/views/dialogue-test";
+// import BattleScreen from "~/views/battle-screen";
+// import { DialogueTestView } from "~/views/dialogue-test";
 import MouseTracker from "~/components/effects/mouse-tracker";
 import InventoryTestView from "~/views/inventory-test";
 import ResourcesTestView from "~/views/resources-test";
 import SoundTestView from "~/views/sound-test";
 import TestView from "~/views/test-view";
 import PartyTestView from "~/views/party-test";
+import TownHub from "~/components/town/town-hub";
 
 function App() {
   return (
   <>
+    <TownHub
+      innCost={{ coins: 10, gold: 0, silver: 0, bronze: 0, copper: 0 }}
+      itemsForSell={["potion"]}
+      onLeaveCallback={() => {}}
+     />
     <TestView />
     <MouseTracker />
     <PartyTestView />
