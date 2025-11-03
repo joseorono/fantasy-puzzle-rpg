1c7fdad04ca05209b45be7aca5a3449f263ddc7b
José Miguel Oroño <joseomaker@gmail.com>
2025-11-03 12:30:30 -0500

    Create links-to-licenses.tsx

---
+++ src/components/licenses/links-to-licenses.tsx
@@ -0,0 +1,21 @@
+export default function LinksToLicensesDialogContent() {
+    return (
+            <div id="links-to-licences" className="flex flex-col gap-2">
+            <h3 className="text-xl">
+                LINKS TO LICENCES:
+            </h3>
+            <p>
+                CC BY 4.0 Deed&nbsp;
+                <a href="https://creativecommons.org/licenses/by/4.0/deed.en">
+                https://creativecommons.org/licenses/by/4.0/deed.en
+                </a>
+            </p>
+            <p>
+                CC0 1.0 Deed&nbsp;
+                <a href="https://creativecommons.org/publicdomain/zero/1.0/">
+                https://creativecommons.org/publicdomain/zero/1.0/
+                </a>
+            </p>
+        </div>
+    );
+}
