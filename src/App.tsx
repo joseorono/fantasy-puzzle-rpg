966731924ab96fca5eaa844f27525523860b22db
José Miguel Oroño <joseomaker@gmail.com>
2025-11-03 03:40:08 -0500

    Refactor App with  new GameScreen component
    Moved the main game-related views from App.tsx into a new GameScreen component for better separation of concerns and maintainability. App.tsx now renders GameScreen and MouseTracker only.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
-// import BattleScreen from "~/views/battle-screen";
 // import { DialogueTestView } from "~/views/dialogue-test";
 import MouseTracker from '~/components/effects/mouse-tracker';
-import InventoryTestView from '~/views/inventory-test';
-import ResourcesTestView from '~/views/resources-test';
-import SoundTestView from '~/views/sound-test';
-import TestView from '~/views/test-view';
-import PartyTestView from '~/views/party-test';
-import TownHub from '~/components/town/town-hub';
-import { DialogueTestView } from './views/dialogue-test';
-import { LevelUpDemo } from './views/level-up-demo';
-import { MapDemo } from './views/map-demo';
+import GameScreen from '~/game-screen';
+
 
 function App() {
   return (
     <>
-      <TownHub
-        innCost={{ coins: 10, gold: 0, silver: 0, bronze: 0, copper: 0 }}
-        itemsForSell={['potion']}
-        onLeaveCallback={() => {}}
-      />
-      <TestView />
       <MouseTracker />
-      <PartyTestView />
-      <InventoryTestView />
-      <ResourcesTestView />
-      <SoundTestView />
-      {/* <LevelUpDemo /> */}
-      {/* <MapDemo /> */}
-      {/* <DialogueTestView /> */}
-      {/* <BattleScreen /> */}
+      <GameScreen />
     </>
-  );
+  )
 }
 
 export default App;
---
+++ src/game-screen.tsx
@@ -0,0 +1,32 @@
+import InventoryTestView from '~/views/inventory-test';
+import ResourcesTestView from '~/views/resources-test';
+import SoundTestView from '~/views/sound-test';
+import TestView from '~/views/test-view';
+import PartyTestView from '~/views/party-test';
+import TownHub from '~/components/town/town-hub';
+//import { DialogueTestView } from './views/dialogue-test';
+//import { LevelUpDemo } from './views/level-up-demo';
+//import { MapDemo } from './views/map-demo';
+// import BattleScreen from "~/views/battle-screen";
+
+
+export default function GameScreen() {
+  return (
+    <>
+      <TownHub
+        innCost={{ coins: 10, gold: 0, silver: 0, bronze: 0, copper: 0 }}
+        itemsForSell={['potion']}
+        onLeaveCallback={() => {}}
+      />
+      <TestView />
+      <PartyTestView />
+      <InventoryTestView />
+      <ResourcesTestView />
+      <SoundTestView />
+      {/* <LevelUpDemo /> */}
+      {/* <MapDemo /> */}
+      {/* <DialogueTestView /> */}
+      {/* <BattleScreen /> */}
+    </>
+  );
+}
