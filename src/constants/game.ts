4d8f5f3015550d691177024433cf5bee94600e72
José Miguel Oroño <joseomaker@gmail.com>
2026-06-12 20:24:27 -0500

    Update game.ts

--- src/constants/game.ts
+++ src/constants/game.ts
@@ -13,6 +13,12 @@ export const STAT_TYPES: StatType[] = ['pow', 'vit', 'spd'];
 // Match configuration
 export const MIN_MATCH_LENGTH = 3;
 
+/** A line match of this size or larger spawns a wildcard bomb in the refill. */
+export const BOMB_MATCH_SPAWN_THRESHOLD = 5;
+
+/** Chance (0-1) that any individual refilled orb spawns as a wildcard bomb. */
+export const BOMB_REFILL_CHANCE = 0.02;
+
 /** Minimum time (ms) the loading screen is shown, regardless of actual load speed. */
 export const MIN_LOAD_TIME_MS = 800;
 
