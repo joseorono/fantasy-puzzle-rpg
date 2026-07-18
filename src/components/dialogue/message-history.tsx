fc122e2d17fbfecc7fa629bf91dd81cd3038b0a6
José Miguel Oroño <joseomaker@gmail.com>
2026-07-18 05:34:29 -0500

    turn mh-title into standard heading

--- src/components/dialogue/message-history.tsx
+++ src/components/dialogue/message-history.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import type { DialogueLine, DialogueCharacter } from '~/types/dialogue';
 import { MAX_MESSAGE_HISTORY } from '~/constants/dialogue';
 import { ToffecCloseButton } from '~/components/ui-custom/toffec-close-button';
+import { ModalTitle } from '~/components/typography/modal-title';
 
 interface MessageHistoryProps {
   lines: DialogueLine[];
@@ -42,7 +43,7 @@ export function MessageHistory({ lines, characters, currentIndex, isOpen, onClos
       {/* Message History Container */}
       <div className="mh-container" ref={containerRef}>
         <div className="mh-header">
-          <h2 className="mh-title">Message History</h2>
+          <ModalTitle text="Message History" className="mh-title" />
           <ToffecCloseButton variant="medieval1" hasBg size="sm" onClick={onClose} />
         </div>
 
