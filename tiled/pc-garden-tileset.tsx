8d7e94382bfdd361ef1d29701b125963b5e0a990
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2026-09-20 20:04:55 -0400

    Upload tiled files

---
+++ tiled/pc-garden-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pc-garden-tileset" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="../public/assets/tileset/pc-garden-tileset.png" width="480" height="480"/>
+</tileset>
